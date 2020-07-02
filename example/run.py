131a9f9e5a256d887998c242edeb1cf15183a319
Filip <fdabek1@umbc.edu>
2020-07-01 22:47:08 -0400

    Cleaning pipeline for each execution.

--- example/run.py
+++ example/run.py
@@ -2,4 +2,5 @@ from rura.logic.runner import Runner
 
 if __name__ == '__main__':
     runner = Runner('sample.yaml', analysis='sample')
+    runner.clean_pipeline('simple')
     runner.execute_pipeline('simple')
